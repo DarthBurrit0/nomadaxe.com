067a3bad7def19a71fba3330c497e5e6b454ace4
Jason Campbell <jason@greatergood.cc>
2012-04-26 11:15:40 -0700

    render a background

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
 
-})
+var canvas = document.getElementById('game')
+  , context = canvas.getContext('2d')
+  , backgroundReady
+  , backgroundIMG = new Image()
+;
+
+backgroundIMG.onload = function(){
+  backgroundReady = true
+}
+
+backgroundIMG.src = '/images/background.png'
+
+var render = function(){
+  if (backgroundReady) context.drawImage(backgroundIMG, 0, 0, 512, 480)
+}
+
+var main = function(){
+  var then = then || Date.now()
+  var now = Date.now()
+  var delta = now - then
+
+  // update(delta / 1000)
+  render();
+
+  then = now;
+}
+
+setInterval(main, 1)
