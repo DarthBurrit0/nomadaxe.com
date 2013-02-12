e25a636b9280f7243cec535db6a9d1252ad6d451
Jason Campbell <jason@js.la>
2013-02-12 00:21:28 -0800

    Adds shoe/socks

--- browser/client.js
+++ browser/client.js
@@ -1,6 +1,12 @@
 
 var domready = require('domready')
+  , shoe = require('shoe')
+  , sock = shoe('/sock')
 
 domready(function(){
   console.log('Where am I?')
+
+  sock.on('data', function(data){
+    console.log(data)
+  })
 })
